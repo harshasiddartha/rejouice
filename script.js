138f40b57a2876edd41e3575f5bc70719bd2f059
siddartha018 <siddartha2367@gmail.com>
2024-06-23 20:25:40 +0530

    corrected the code

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const locoScroll = new LocomotiveScroll({
   smooth: true,
 
   // for tablet smooth
- // tablet: { smooth: true },
+  tablet: { smooth: true },
 //for amazing and excellent performance
   // for mobile
-  //smartphone: { smooth: true }
+  smartphone: { smooth: true }
 });
 locoScroll.on("scroll", ScrollTrigger.update);
 
